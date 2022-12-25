b2d7722a0e3241cedb10e0365e9840cbabd0456f
Geordie King <geordie.king@gmail.com>
2022-12-25 17:54:25 +0000

    sql/prisma changes to contant table

--- config/SQL/CREATE - Table - w2w.Show.sql
+++ config/SQL/CREATE - Table - w2w.Show.sql
@@ -8,7 +8,7 @@
 USE [randoSmallProjects]
 GO
 
---DROP TABLE IF EXISTS [w2w].[Show]
+--DROP TABLE IF EXISTS [w2w].[Content]
 --GO
 
 SET ANSI_NULLS ON
@@ -17,11 +17,11 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [w2w].[Show](
-  [ShowID] INT IDENTITY(1,1) NOT NULL,
-  [ShowName] VARCHAR(100) NOT NULL,
-	[ShowAKA]  VARCHAR(100) NULL,
-  [ShowType] VARCHAR(5) NOT NULL,
+CREATE TABLE [w2w].[Content](
+  [ContentID] INT IDENTITY(1,1) NOT NULL,
+  [ContentName] VARCHAR(100) NOT NULL,
+	[ContentAKA]  VARCHAR(100) NULL,
+  [ContentType] VARCHAR(5) NOT NULL,
   [Seasons] INT NULL,
 	[Genre] VARCHAR(100) NOT NULL,
 	[Provider] VARCHAR(25) NOT NULL,
@@ -37,25 +37,25 @@ CREATE TABLE [w2w].[Show](
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [w2w].[Show]
-	ADD PRIMARY KEY(ShowID)
+ALTER TABLE [w2w].[Content]
+	ADD PRIMARY KEY(ContentID)
 GO
 
-ALTER TABLE [w2w].[Show]
-  ADD CONSTRAINT [DF_w2wShow_CreatedDateTime_SetToCurrentDateTime]  
+ALTER TABLE [w2w].[Content]
+  ADD CONSTRAINT [DF_w2wContent_CreatedDateTime_SetToCurrentDateTime]  
   DEFAULT (GETDATE()) FOR [CreatedDateTime]
 GO
 
-ALTER TABLE [w2w].[Show]
-  ADD CONSTRAINT [DF_w2wShow_EOL_SetTo0]  
+ALTER TABLE [w2w].[Content]
+  ADD CONSTRAINT [DF_w2wContent_EOL_SetTo0]  
   DEFAULT (0) FOR [EOL]
 GO
 
-ALTER TABLE [w2w].[Show]
-  ADD CONSTRAINT [DF_w2wShow_CurrentlyWatching_SetTo0]  
+ALTER TABLE [w2w].[Content]
+  ADD CONSTRAINT [DF_w2wContent_CurrentlyWatching_SetTo0]  
   DEFAULT (0) FOR [CurrentlyWatching]
 GO
-ALTER TABLE [w2w].[Show]
-  ADD CONSTRAINT [DF_w2wShow_WatchNext_SetTo0]  
+ALTER TABLE [w2w].[Content]
+  ADD CONSTRAINT [DF_w2wContent_WatchNext_SetTo0]  
   DEFAULT (0) FOR [WatchNext]
 GO
